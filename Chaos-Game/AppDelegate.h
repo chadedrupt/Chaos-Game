378423e12821517149f2c4943d62202e9d98bb5c
Chad Edrupt <chad.edrupt@ultrasoft.com.au>
2013-12-23 22:53:01 +1000

    Initial empty template

---
+++ Chaos-Game/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Chaos-Game
+//
+//  Created by Chad Edrupt on 23/12/2013.
+//  Copyright (c) 2013 Ultrasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Chaos-Game/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Chaos-Game
+//
+//  Created by Chad Edrupt on 23/12/2013.
+//  Copyright (c) 2013 Ultrasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
